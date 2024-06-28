b69896ef43591886d7e4059c78741e1044836209
De-miles <miles998321@gmail.com>
2024-06-28 16:02:55 +0700

    handle api errors

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
 import axiosClient from './api/axiosClient'
 import postApi from './api/postApi'
 
-console.log('hello from main.js')
-
 async function main() {
   // const respone = await axiosClient.get('/posts')
-  const queryParams = {
-    _page: 1,
-    _limit: 5,
+  try {
+    const queryParams = {
+      _page: 1,
+      _limit: 5,
+    }
+    const data = await postApi.getAll(queryParams)
+    console.log('main.js data : ', data)
+  } catch (error) {
+    console.log('get All failed', error)
+    // show modal, toast error
   }
-  const data = await postApi.getAll(queryParams)
-  console.log('posts data : ', data)
+
+  // await postApi.updateFormData({
+  //   id: 'aqbbx1vj1lqrtv3wg',
+  //   title: 'Est et rerum 222',
+  // })
 }
 
 main()
