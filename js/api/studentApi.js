870ffa6a4b670219ae226666f1eaf0f12edee53e
De-miles <miles998321@gmail.com>
2024-06-28 13:48:11 +0700

    setup axios - axios client and postAPi

---
+++ js/api/studentApi.js
@@ -0,0 +1,30 @@
+import axiosClient from './axiosClient'
+
+const studentApi = {
+  getAll(params) {
+    const url = '/student'
+    return axiosClient.get(url, { params })
+  },
+
+  getById(id) {
+    const url = `/student/${id}`
+    return axiosClient.get(url)
+  },
+
+  add(data) {
+    const url = '/student'
+    return axiosClient.post(url, data)
+  },
+
+  update(data) {
+    const url = `/student/${data.id}`
+    return axiosClient.patch(url, data)
+  },
+
+  remove(id) {
+    const url = `/student/${id}`
+    return axiosClient.delete(url)
+  },
+}
+
+export default studentApi
--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+import axiosClient from './api/axiosClient'
+import postApi from './api/postApi'
+
 console.log('hello from main.js')
+
+async function main() {
+  // const respone = await axiosClient.get('/posts')
+  const queryParams = {
+    _page: 1,
+    _limit: 5,
+  }
+  const respone = await postApi.getAll(queryParams)
+  console.log('posts: ', respone)
+}
+
+main()
