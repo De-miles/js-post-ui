6d0d445163de5c17eedad1b80ed699013462ad8b
De-miles <miles998321@gmail.com>
2024-06-30 22:44:16 +0700

    refactor code - smart function

--- js/home.js
+++ js/home.js
@@ -1,85 +1,5 @@
-import axiosClient from './api/axiosClient.js'
 import postApi from './api/postApi.js'
-import { getUlPagination, setTextContent, truncateText } from './utils'
-// to use fromNow() func
-import dayjs from 'dayjs'
-import relativeTime from 'dayjs/plugin/relativeTime'
-import debounce from 'lodash.debounce'
-
-dayjs.extend(relativeTime)
-
-function createPostElement(post) {
-  if (!post) return
-
-  // find and clone template
-  const postTemplate = document.getElementById('postTemplate')
-  if (!postTemplate) return
-
-  const liElement = postTemplate.content.firstElementChild.cloneNode(true)
-  if (!liElement) return
-
-  // update title, description, author, thumbnail
-  setTextContent(liElement, '[data-id="title"]', post.title)
-  setTextContent(liElement, '[data-id="description"]', truncateText(post.description, 100))
-  setTextContent(liElement, '[data-id="author"]', post.author)
-
-  // const descriptionElement = liElement.querySelector('[data-id="description"]')
-  // if (descriptionElement) descriptionElement.textContent = post.description
-
-  // const authorElement = liElement.querySelector('[data-id="author"]')
-  // if (authorElement) authorElement.textContent = post.author
-
-  // calculate timespan
-  // console.log('timespan', dayjs(post.updatedAt).fromNow())
-  setTextContent(liElement, '[data-id="timeSpan"]', `- ${dayjs(post.updatedAt).fromNow()}`)
-
-  const thumbnailElement = liElement.querySelector('[data-id="thumbnail"]')
-  if (thumbnailElement) {
-    thumbnailElement.src = post.imageUrl
-
-    thumbnailElement.addEventListener('error', () => {
-      thumbnailElement.src = 'https://via.placeholder.com/1368x400?text=thumbnail'
-    })
-  }
-
-  // attach event
-  return liElement
-}
-
-function renderPostList(postList) {
-  if (!Array.isArray(postList)) return
-
-  const ulElement = document.getElementById('postList')
-  if (!ulElement) return
-
-  // clear current list
-  ulElement.textContent = ''
-
-  postList.forEach((post) => {
-    const liElement = createPostElement(post)
-    ulElement.appendChild(liElement)
-  })
-}
-
-function renderPagination(pagination) {
-  const ulPagination = getUlPagination()
-  if (!pagination || !ulPagination) return
-
-  // calc totalPages
-  const { _page, _limit, _totalRows } = pagination
-  const totalPages = Math.ceil(_totalRows / _limit)
-
-  // save page and totalPages to ulPagination
-  ulPagination.dataset.page = _page
-  ulPagination.dataset.totalPages = totalPages
-
-  // check if enable/disable prev/next links
-  if (_page <= 1) ulPagination.firstElementChild?.classList.add('disabled')
-  else ulPagination.firstElementChild?.classList.remove('disabled')
-
-  if (_page >= totalPages) ulPagination.lastElementChild?.classList.add('disabled')
-  else ulPagination.lastElementChild?.classList.remove('disabled')
-}
+import { initPagination, initSearch, renderPostList, renderPagination } from './utils'
 
 async function handleFilterChange(filterName, filterValue) {
   try {
@@ -95,76 +15,14 @@ async function handleFilterChange(filterName, filterValue) {
     // fetch API
     const { data, pagination } = await postApi.getAll(url.searchParams)
     // re-render post list
-    renderPostList(data)
-    renderPagination(pagination)
+    renderPostList('postList', data)
+    renderPagination('pagination', pagination)
   } catch (error) {
     console.log('failed to fetch post list', error)
   }
 }
 
-function handlePrevLink(e) {
-  e.preventDefault()
-
-  const ulPagination = getUlPagination()
-  if (!ulPagination) return
-
-  const page = Number.parseInt(ulPagination.dataset.page) || 1
-  if (page <= 1) return
-
-  handleFilterChange('_page', page - 1)
-}
-
-function handleNextLink(e) {
-  e.preventDefault()
-
-  const ulPagination = getUlPagination()
-  if (!ulPagination) return
-
-  const page = Number.parseInt(ulPagination.dataset.page) || 1
-  const totalPages = ulPagination.dataset.totalPages
-
-  if (page >= totalPages) return
-
-  handleFilterChange('_page', page + 1)
-}
-
-function initPagination() {
-  // bind click event for pre/next link
-  const ulPagination = getUlPagination()
-  if (!ulPagination) return
-
-  // add click event for prev link
-  const prevLink = ulPagination.firstElementChild?.firstElementChild
-  if (prevLink) {
-    prevLink.addEventListener('click', handlePrevLink)
-  }
-
-  // add click event for next link
-  const nextLink = ulPagination.lastElementChild?.lastElementChild
-  if (nextLink) {
-    nextLink.addEventListener('click', handleNextLink)
-  }
-}
-
-function initSearch() {
-  const searchInput = document.getElementById('searchInput')
-  if (!searchInput) return
-
-  // set default values from query params
-  // title_like
-  const queryParams = new URLSearchParams(window.location.search)
-  if (queryParams.get('title_like')) {
-    searchInput.value = queryParams.get('title_like')
-  }
-
-  const debounceSearch = debounce(
-    (event) => handleFilterChange('title_like', event.target.value),
-    500,
-  )
-
-  searchInput.addEventListener('input', debounceSearch)
-}
-
+// MAIN
 ;(async () => {
   try {
     const url = new URL(window.location)
@@ -176,12 +34,21 @@ function initSearch() {
     history.pushState({}, '', url)
     const queryParams = url.searchParams
 
-    initPagination(queryParams)
-    initSearch(queryParams)
+    initPagination({
+      elementId: 'pagination',
+      defaultParams: queryParams,
+      onChange: (page) => handleFilterChange('_page', page),
+    })
+
+    initSearch({
+      elementId: 'searchInput',
+      defaultParams: queryParams,
+      onChange: (value) => handleFilterChange('title_like', value),
+    })
 
     const { data, pagination } = await postApi.getAll(queryParams)
-    renderPostList(data)
-    renderPagination(pagination)
+    renderPostList('postList', data)
+    renderPagination('pagination', pagination)
   } catch (error) {
     console.log('get all failed', error)
     // show modal, toast error
